cb9a61d6a5cb4c85294fc905d6e336352aac4773
Eliott Guignabaudet <eliott.guignabaudet@gmail.com>
2023-03-03 23:51:09 +0100

    add remaining time, remaining time UI and game over panel

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI RemainingTimeUI;
+    public GameObject GameOverPanel;
+
+    private float _remainingTime;
+
+    private bool isEnd;
+
+    public float RemainingTime
+    {
+        get { return _remainingTime; }
+        set { 
+            _remainingTime = value;
+            RemainingTimeUI.text = value.ToString("0.00");
+        }
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RemainingTime = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (isEnd)
+        {
+            return;
+        }
+        RemainingTime -= Time.deltaTime;
+        if (RemainingTime <= 0)
+        {
+            GameOver();
+            return;
+        }
+    }
+
+    public void GameOver()
+    {
+        RemainingTime = 0;
+        isEnd = true;
+        GameOverPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
