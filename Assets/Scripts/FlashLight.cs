7d0137cdcfbea1fed325e7a7433d2fba461d77ed
AlbaNagisa <alban.girardi@ynov.com>
2023-03-04 11:17:19 +0100

    adding audio + 3d model

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class FlashLight : MonoBehaviour
 {
     private bool _isOn = true;
+    [SerializeField] private GameObject lightSource;
+    private AudioSource _audioSource;
+    private void Start()
+    {
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
     public bool IsOn
     {
         get { return _isOn;}
         set { _isOn = value; }
     }
-    [SerializeField] private GameObject lightSource;
     public void SwitchTorch()
     {
+        _audioSource.Play();
         lightSource.SetActive(IsOn);
     }
   
